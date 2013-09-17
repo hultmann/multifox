d46b0bb9c9452de7c5bc44477b453c63c8eaf334
Jeferson Hultmann <hultmann@gmail.com>
2013-09-17 18:23:57 -0300

    v. 2.0.4

--- build.py
+++ build.py
@@ -46,7 +46,7 @@ b.add_locale("pl")
 b.add_text("locale/${locale}/extension.properties")
 b.add_text("locale/${locale}/about-multifox.properties")
 
-b.set_var("EXT_VERSION", "2.0.4b1")
+b.set_var("EXT_VERSION", "2.0.4")
 verEx = build_tools.getVersionedString(changeset, b.get_var("EXT_VERSION"))
 
 if changeset == None:
