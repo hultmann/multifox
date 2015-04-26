01215a6d6923675de40c5aff13853829b8ace02b
Jeferson Hultmann <hultmann@gmail.com>
2015-04-26 14:21:42 -0300

    Version bump (3.2.1b2)

--- build.py
+++ build.py
@@ -51,7 +51,7 @@ b.add_text("locale/${locale}/options.dtd")
 b.add_text("locale/${locale}/extension.properties")
 b.add_text("locale/${locale}/about-multifox.properties")
 
-b.set_var("EXT_VERSION", "3.2.1b1")
+b.set_var("EXT_VERSION", "3.2.1b2")
 verEx = build_tools.getVersionedString(changeset, b.get_var("EXT_VERSION"))
 
 if changeset == None:
